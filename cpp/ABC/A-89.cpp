ab57413116532d834433acd9cb62612ebadb191b
abap34 <uchi_ymgc@her0.jp>
2020-03-30 21:33:13 +0900

    solve problems of the past contest

---
+++ cpp/ABC/A-89.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+
+int main(){
+    int N;
+    cin >> N;
+    cout << (N/3) << endl;
+}
---
+++ cpp/ABC/A-90.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+
+int main(){
+    string S1,S2,S3;
+    cin >> S1;
+    cin >> S2;
+    cin >> S3;
+    cout << S1[0];
+    cout << S2[1];
+    cout << S3[2];
+    cout << endl;
+}
