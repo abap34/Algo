8978a6779d763eac6cfabfca0bdb5778fada7b0f
abap34 <uchi_ymgc@her0.jp>
2020-09-01 23:59:28 +0900

    Solve problems of the past contests

--- PCK/pre_2009/D.cpp
+++ PCK/pre_2009/D.cpp
@@ -3,53 +3,75 @@
 using namespace std;
 using Graph = vector<vector<int>>;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define ALL(a)  (a).begin(),(a).end()
+#define ALL(a) (a).begin(), (a).end()
 
-template <typename T>
-void cout_vector(vector <T> A){
-    for (T a : A) {
-        for (auto aa : a){
-            cout << aa << " ";
-        }
-        cout << endl;
-    }
-cout << endl;
+
+void turn_side(vector<string> &box)
+{
+    string tmp;
+    tmp = box[3];
+    box[3] = box[5];
+    box[5] = box[2];
+    box[2] = box[0];
+    box[0] = tmp;
 }
 
-template <typename T>
-void cout_set(set <T> A){
-    for (T a : A) {
-        for (auto aa : a){
-            cout << aa << " ";
-        }
-        cout << endl;
-    }
-cout << endl;
+void turn_vertical(vector<string> &box)
+{
+    string tmp;
+    tmp = box[0];
+    box[0] = box[4];
+    box[4] = box[5];
+    box[5] = box[1];
+    box[1] = tmp;
 }
 
-int main(){
-    cout << "===========" << endl;
+int main()
+{
     int n;
-    while (true){
+    while (true)
+    {
         cin >> n;
-        if (n == 0) break;
-        vector<vector<string>> colors(n, vector<string>(6));
-        rep(i, n){
-            rep(j, 6){
-                cin >> colors[i][j];
+        if (n == 0)
+            break;
+        vector<vector<string>> boxes(n, vector<string>(6));
+
+        rep(i, n)
+        {
+            rep(j, 6)
+            {
+                cin >> boxes[i][j];
             }
         }
-        cout << "---" << endl;
-        cout << "input:" << endl;
-        cout_vector(colors);
-        cout << "unique:" << endl;
-        set<vector<string>> unique_colors(ALL(colors));
-        cout_set(unique_colors);
-        cout << "result:" << endl;
-        cout << (n - unique_colors.size()) << endl;
-        cout << "----------" << endl;
+
+        int duplicate_count = 0;
+
+        rep(i, n)
+        {
+            set<pair<int, int>> seen_pair;
+            vector<string> box = boxes[i];
+            rep(t, 4)
+            {
+                rep(y, 4)
+                {
+                    rep(_, y) turn_side(box);
+                    rep(_, t) turn_vertical(box);
+                    rep(j, n)
+                    {
+                        if (i != j && box == boxes[j])
+                        {
+                            pair<int, int> same_pair = make_pair(i, j);
+                            if (seen_pair.find(same_pair) == seen_pair.end())
+                            {
+                                seen_pair.insert(same_pair);
+                                duplicate_count += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        cout << duplicate_count / 2 << endl;
     }
-
-
-
 }
