a32a414bd4c0a3c406a6f9e961d41d2d83a6b338
abap34 <uchi_ymgc@her0.jp>
2020-10-04 01:27:08 +0900

    solve problems of the past contests

--- julia/other/Educational-DP-Contest/A.jl
+++ julia/other/Educational-DP-Contest/A.jl
@@ -2,19 +2,13 @@ parseInt(x) = parse(Int, x)
 parseMap(x::Array{SubString{String},1}) = map(parseInt, x)
 
 function main()
-    INF = 10 ^ 5
     N = parseInt(readline())
     H = parseMap(split(readline()))
-    dp = fill(INF,N)
+    dp = zeros(Int, 10^5)
     dp[1] = 0
-    for i in 2:N
-        if i > 2
-            o = dp[i-1] + abs(H[i-1] - H[i])
-            t = dp[i-2] + abs(H[i-2] - H[i])
-            dp[i] = min(o,t)
-        else
-            dp[i] = dp[i-1] + abs(H[i-1] - H[i])
-        end
+    dp[2] = abs(H[2] - H[1])
+    for i in 3:N
+        dp[i] = min(dp[i - 2] + abs(H[i] - H[i - 2]), dp[i - 1] + abs(H[i] - H[i - 1]))
     end
     println(dp[N])
 end
