bb9b9d93f15542fb033406d01729a85be25ac890
abap34 <uchi_ymgc@her0.jp>
2020-11-23 10:42:39 +0900

    add searchmax and divide

--- library.jl
+++ library.jl
@@ -7,3 +7,35 @@ function count_combinations(N, K)
 end
 
 
+function divide(A::Array{T, 1}, N::T) where T
+    results = Array{Array{T, 1}, 1}(undef, N)
+    div_range = cld(length(A), N) 
+    for (i, s_idx) in zip(1:N, 1:div_range:length(A))
+        e_idx = min(N, s_idx + div_range)
+        results[i] = A[s_idx:e_idx]
+    end
+    return results
+end
+
+
+function searchmax(A::AbstractArray{T}, key::T)
+    #=
+    Aの中からkey以下の最小の値を返す. 
+    任意のA_iについて、A_i > keyならば、-1を返す
+    =#
+    left = 1
+    right = length(N)
+    while (left < right)
+        mid = left + right
+        if A[mid] > key
+            right = mid
+        elseif A[mid] < key
+            left = mid
+        else
+            return mid
+        end
+    end
+    return mid
+end
+
+
