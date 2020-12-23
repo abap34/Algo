3ae60fe2d3246f285e2ba241f273a1226ffb3cd4
abap34 <uchi_ymgc@her0.jp>
2020-12-23 23:18:14 +0900

    Update library.jl

--- library.jl
+++ library.jl
@@ -38,4 +38,42 @@ function searchmax(A::AbstractArray{T}, key::T)
     return mid
 end
 
+struct UnionFind
+    par :: Array{Int, 1}
+    size :: Array{Int, 1}
+    UnionFind(N) = new(collect(1:N), collect(1:N))
+end
+
+function root(uf::UnionFind, x::Int)
+    if uf.par[x] == x
+        return x
+    else
+        return uf.par[x] = root(uf, uf.par[x])
+    end
+end
+
+
+function issame(uf::UnionFind, x::Int, y::Int)
+    return root(uf, x) == root(uf, y)
+end
+
+function unite(uf::UnionFind, x::Int, y::Int)
+    x = root(uf, x)
+    y = root(uf, y)
+    (x == y) && (return true)
+    if (uf.size[x] < uf.size[y]) 
+        uf.par[x] = y
+        uf.size[y] += uf.size[x]
+    else
+        uf.par[y] = x
+        uf.size[x] += uf.size[y]
+    end
+    return true
+end
+
+
+struct Permutations{T}
+    a::T
+    t::Int
+end
 
