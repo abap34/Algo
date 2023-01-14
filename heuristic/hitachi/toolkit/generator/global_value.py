71102c89b0c3614526890bdbd93d721c6be72514
Yuchi Yamaguchi <abap0002@gmail.com>
2023-01-14 19:42:38 +0900

    rm

--- heuristic/hitachi/toolkit/generator/global_value.py
+++
@@ -1,5 +0,0 @@
-static_value_map = {}
-static_index_map = {}
-T_max = None
-worker_num = None
-map_max_depth = None
--- heuristic/hitachi/toolkit/judge/json_fwd.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef HEADER_2HC2022_JSON_FWD
-#define HEADER_2HC2022_JSON_FWD 
-#include "lib/json_fwd.hpp"
-using json = nlohmann::json;
-using json_ref = json&;
-#endif
